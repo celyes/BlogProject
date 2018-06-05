ea3f0c91acdc46b3e4e12d0a1e356e520778c636
Celyes <celyes02@gmail.com>
2018-06-05 19:38:04 +0100

    Fix navabar links

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,8 +1,13 @@
 <header class="blog-header py-3">
     <div class="row flex-nowrap justify-content-between align-items-center">
       <div class="col-4 pt-1">
+        @if(Auth::check())
+        <a class="btn btn-sm btn-outline-secondary" href="/logout">Log out</a>
+        <a class="btn-sm" href="/profile">{{ Auth::user()->name }}</a>
+        @else
         <a class="btn btn-sm btn-primary" href="/register">Register</a>
         <a class="btn btn-sm btn-outline-secondary" href="/login">login</a>
+        @endif
       </div>
       <div class="col-4 text-center">
         <a class="blog-header-logo text-dark" href="/posts">Laravel project</a>
